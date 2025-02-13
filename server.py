723a5b53bee3a1ec4f17470000b3b2c40077f0b1
samTime101 <srijanregmi44@gmail.com>
2025-02-13 14:19:10 +0545

    cloudfare

--- server.py
+++ server.py
@@ -27,4 +27,4 @@ def guess():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True , allow_unsafe_werkzeug=True)
