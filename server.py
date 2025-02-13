427f58513764833f140bd692a77f0f9dc65fe6cb
samTime101 <srijanregmi44@gmail.com>
2025-02-13 13:33:37 +0545

    working model

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify , request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
@@ -15,21 +15,16 @@ window.history.back()
 
 usr_choice = 0  
 
-@app.route('/guess/<guess>', methods=['GET'])
-def guess(guess):
-    if guess.isdigit():
-        correct = int(guess) == usr_choice
-        return render_template('guess.html', guess=guess, correct=correct, choose=usr_choice)
-    return INTEGER_ERROR
+@app.route('/play/', methods=['GET', 'POST'])
+def guess():
+    if request.method == 'GET':
+        return render_template('guess.html', guess="")
+    
+    data = request.get_json()
+    table_num = data.get("clicked", "").strip()
+    socketio.emit('clicked', {'table_num': table_num})
+    # return INTEGER_ERROR
 
-@app.route('/choose/<choose>', methods=['GET'])
-def choose(choose):
-    global usr_choice
-    if choose.isdigit():
-        usr_choice = int(choose)
-        socketio.emit('update_usr_choice', {'usr_choice': usr_choice})
-        return render_template('choose.html', choose=choose)
-    return INTEGER_ERROR
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
